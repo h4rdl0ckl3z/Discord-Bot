86b61d83c9f5d85285bcb4405723e95929dfc3ba
h4rdl0ckl3z <bank427ingza@gmail.com>
2024-01-22 20:04:35 +0700

    SleepZ

---
+++ events/status.js
@@ -0,0 +1,16 @@
+const { ActivityType } = require('discord.js');
+
+exports.status = [
+    {
+        name: 'Custom1',
+        type: ActivityType.Custom
+    },
+    {
+        name: 'Custom2',
+        type: ActivityType.Custom
+    },
+    {
+        name: 'Custom3',
+        type: ActivityType.Custom
+    }
+];
